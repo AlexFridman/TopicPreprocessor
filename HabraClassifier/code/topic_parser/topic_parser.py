e78cf3429a6819824fc8fbadd16ec3ea943c0ebd
Alex Fridman <AlexFridman@outlook.com>
2015-12-11 13:23:56 +0300

    Add name extraction in TopicParser

--- HabraClassifier/code/topic_parser/topic_parser.py
+++ HabraClassifier/code/topic_parser/topic_parser.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 from bs4 import BeautifulSoup
 
-Topic = namedtuple('Topic', ['text', 'hubs', 'tags'])
+Topic = namedtuple('Topic', ['text', 'hubs', 'tags', 'name'])
 
 
 class TopicParser:
@@ -15,13 +15,19 @@ class TopicParser:
         tags = TopicParser.extract_tags(soup)
         hubs = TopicParser.extract_hubs(soup)
         text = TopicParser.extract_text(soup)
+        name = TopicParser.extract_name(soup)
 
-        return Topic(text=text, hubs=hubs, tags=tags)
+        return Topic(text=text, hubs=hubs, tags=tags, name=name)
 
     @staticmethod
     def extract_tags(soup: BeautifulSoup) -> list:
         tag_tags = soup.findAll(name='a', attrs={'rel': 'tag'})
         return [tag_tag.get_text().lower() for tag_tag in tag_tags]
+
+    @staticmethod
+    def extract_name(soup: BeautifulSoup) -> list:
+        tag_tags = soup.find(name='span', attrs={'class': 'post_title'})
+        return tag_tags.get_text()
 
     @staticmethod
     def extract_hubs(soup: BeautifulSoup) -> list:
